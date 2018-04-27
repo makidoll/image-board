5c510e4db2d431ce9581e53f444ae29e55b9f4bb
Maki <mxmcube@gmail.com>
2018-04-27 15:40:35 +0100

    Database folder automatically made

--- modules/database.js
+++ modules/database.js
@@ -1,16 +1,17 @@
 var Datastore = require("nedb");
+var fs = require("fs");
+
+let dir = global.__dirname+"/database";
+if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 
 global.database = {};
-function loadBoard(name) {
-	global.database[name] = new Datastore(global.__dirname+"/database/"+name+".db")
-	global.database[name].loadDatabase();
-
-	global.debugLog("Loading database: "+name);
-}
 
 let boards = Object.keys(global.settings.boards);
 for (var i = 0; i < boards.length; i++) {
 	let name = boards[i];
 	let board = global.settings.boards[name];
-	loadBoard(name);
+
+	global.database[name] = new Datastore(global.__dirname+"/database/"+name+".db")
+	global.database[name].loadDatabase();
+	global.debugLog("Loaded database: "+name);
 }
